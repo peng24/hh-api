e7850dd2b4407495e503cd409859e3c02ae4e193
Yanwit Ninrattananon <peng24@gmail.com>
2019-08-09 15:37:40 +0700

    แก้กลับมาเป็นตัวต้นแบบ

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,23 +5,23 @@ export class UserModel {
     return db("register").insert(data);
   }
 
-  // updateDeviceToken(db: Knex, cid: any, deviceToken: any) {
-  //   return db('register')
-  //     .where('cid', cid)
-  //     .update({
-  //       device_token: deviceToken
-  //     });
-  // }
+  updateDeviceToken(db: Knex, cid: any, deviceToken: any) {
+    return db("register")
+      .where("cid", cid)
+      .update({
+        device_token: deviceToken
+      });
+  }
 
-  // checkPincode(db: Knex, cid: any, pincode: any) {
-  //   return db('register')
-  //     .where('cid', cid)
-  //     .where('pincode', pincode);
-  // }
+  checkPincode(db: Knex, cid: any, pincode: any) {
+    return db("register")
+      .where("cid", cid)
+      .where("pincode", pincode);
+  }
 
-  // getDeviceToken(db: Knex, registerId: any) {
-  //   return db('register')
-  //     .select('device_token')
-  //     .where('register_id', registerId);
-  // }
+  getDeviceToken(db: Knex, registerId: any) {
+    return db("register")
+      .select("device_token")
+      .where("register_id", registerId);
+  }
 }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,6 +5,7 @@ import { Jwt } from "../models/jwt";
 import { UserModel } from "../models/user";
 
 import * as HttpStatus from "http-status-codes";
+import { NextFunction } from "connect";
 
 const jwt = new Jwt();
 const userModel = new UserModel();
@@ -13,7 +14,7 @@ const router: Router = Router();
 
 // localhost:3000/user/save
 router.post("/save", async (req: Request, res: Response) => {
-  var pincode = req.body.pincode;
+  var pincode = req.body.pincode; // hn
   var telephone = req.body.telephone;
   var cid = req.body.cid;
   var emi = req.body.emi;
@@ -36,5 +37,41 @@ router.post("/save", async (req: Request, res: Response) => {
     res.send({ ok: false, message: "ข้อมูลไม่ครบ" });
   }
 });
+
+// localhost:3000/user/login
+router.post(
+  "/login",
+  async (req: Request, res: Response, next: NextFunction) => {
+    var pincode = req.body.pincode;
+    var cid = req.body.cid;
+    var deviceToken = req.body.deviceToken || null;
+
+    if (pincode && cid) {
+      try {
+        var rs: any = await userModel.checkPincode(req.db, cid, pincode);
+        if (rs.length) {
+          var registerId = rs[0].register_id;
+
+          var payload = {
+            id: registerId
+          };
+
+          if (deviceToken) {
+            await userModel.updateDeviceToken(req.db, cid, deviceToken);
+          }
+
+          var token = await jwt.sign(payload);
+          res.send({ ok: true, token: token });
+        } else {
+          res.send({ ok: false, message: "Login failed!" });
+        }
+      } catch (error) {
+        res.send({ ok: false, message: error.message });
+      }
+    } else {
+      res.send({ ok: false, message: "ข้อมูลไม่ครบ" });
+    }
+  }
+);
 
 export default router;
